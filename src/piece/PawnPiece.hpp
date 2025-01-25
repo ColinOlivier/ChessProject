f644bc1ac1ef50ded16d5b8bbf7def18a6fe1ef5
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-01-25 10:18:15 +0100

    ✨Add all Piece structure in .hpp

---
+++ src/piece/PawnPiece.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "AbstractPiece.hpp"
+class PawnPiece : public AbstractPiece {
+};
---
+++ src/piece/QueenPiece.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "AbstractPiece.hpp"
+class QueenPiece : public AbstractPiece {
+};

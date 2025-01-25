6cc17efea73284a0cb870e226e009f474ff1f24f
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-01-25 12:55:57 +0100

    ✨ Update Tile header with method definition

--- src/BoardTile.hpp
+++ src/BoardTile.hpp
@@ -9,4 +9,11 @@ class BoardTile {
     PositionTile   m_position;
     Board*         m_boardPtr;
     AbstractPiece* m_piecePtr;
+
+public:
+    PositionTile getPosition() const;
+    unsigned int x() const;
+    unsigned int y() const;
+
+    AbstractPiece* getPiece() const;
 };

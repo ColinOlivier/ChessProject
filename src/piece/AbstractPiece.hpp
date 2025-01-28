83cdfde8b6cbd99004cc62a7f22c796efd0a9950
eliWagn <wagnerelisab18@gmail.com>
2025-01-28 11:44:48 +0100

    ✨Add setter and getter protected in AbstractPiece

--- src/piece/AbstractPiece.hpp
+++ src/piece/AbstractPiece.hpp
@@ -15,9 +15,12 @@ class AbstractPiece {
     PlayerColor m_playerColor;
     BoardTile*  m_tilePtr;
 
-public:
-    PlayerColor  getPlayerColor() const;
-    PositionTile getPosition() const;
+protected:
+    void       setTile(BoardTile* tile) { m_tilePtr = tile; }
+    void       setPlayerColor(PlayerColor color) { m_playerColor = color; }
+    BoardTile* getTile() const { return m_tilePtr; }
 
+public:
+    PlayerColor                     getPlayerColor() const { return m_playerColor; }
     virtual std::vector<BoardTile*> getPossibleMove();
 };

9ccf13324073adf20c972fb6610e156874dea665
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-02-04 16:30:56 +0100

    ✨Implement Board and BoardTile classes with initialization and piece management methods

---
+++ src/Board.cpp
@@ -0,0 +1,42 @@
+#include "Board.hpp"
+#include <memory>
+#include <stdexcept>
+#include "BoardTile.hpp"
+#include "PositionTile.hpp"
+#include "piece/AbstractPiece.hpp"
+#include "piece/KnightPiece.hpp"
+#include "piece/PawnPiece.hpp"
+
+void Board::initialize()
+{
+    for (size_t y{0}; y < 8; ++y)
+    {
+        for (size_t x{0}; x < 8; ++x)
+        {
+            m_tiles[x + y * 8] = BoardTile(*this, x, y);
+        }
+    }
+
+    createPiece<PawnPiece>(PositionTile(1, 0), PlayerColor::White);
+    createPiece<KnightPiece>(PositionTile(1, 2), PlayerColor::Black);
+}
+
+BoardTile& Board::getTile(PositionTile position)
+{
+    return getTile(position.x, position.y);
+}
+
+const BoardTile& Board::getTile(PositionTile position) const
+{
+    return getTile(position);
+}
+
+BoardTile& Board::getTile(unsigned int x, unsigned int y)
+{
+    return m_tiles[x + y * 8];
+}
+
+const BoardTile& Board::getTile(unsigned int x, unsigned int y) const
+{
+    return m_tiles[x + y * 8];
+}
---
+++ src/BoardTile.cpp
@@ -0,0 +1,20 @@
+#include "BoardTile.hpp"
+#include "PositionTile.hpp"
+
+BoardTile::BoardTile(Board& board, unsigned int x, unsigned int y)
+    : m_boardPtr(&board), m_piecePtr(nullptr)
+{
+    m_position = PositionTile(x, y);
+}
+
+std::string BoardTile::getLabel() const
+{
+    if (m_piecePtr == nullptr)
+        return "##";
+    return m_piecePtr->getLabel();
+};
+
+void BoardTile::setPiece(AbstractPiece& piece)
+{
+    m_piecePtr = &piece;
+}

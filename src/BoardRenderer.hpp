320ba41a26f245461803a0a1e4c9a971a4a5cece
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2025-02-10 13:37:30 +0100

    ✨ Update BoardRenderer to use customizable rendering parameters for improved flexibility

--- src/BoardRenderer.hpp
+++ src/BoardRenderer.hpp
@@ -3,15 +3,18 @@
 #include <imgui.h>
 #include <string>
 #include "Board.hpp"
+#include "BoardRendererParams.hpp"
 #include "BoardTile.hpp"
 
 class BoardRenderer {
-    ImVec4 boardColor1{0.9, 0.9, 0.7, 1.0};
-    ImVec4 boardColor2{1.0, 0.5, 0.5, 1.0};
+private:
+    BoardRendererParams params;
+    ImVec4              boardColor1{0.9, 0.9, 0.7, 1.0};
+    ImVec4              boardColor2{1.0, 0.5, 0.5, 1.0};
 
 public:
     void renderBoard(const Board& board) const;
-    void renderTile(const BoardTile& tile) const;
+    void renderTile(const BoardTile& tile, bool isSelectable) const;
 
     std::string getPieceLabel(const AbstractPiece& piece) const;
 };
